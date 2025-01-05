33a450d8fb5145861f146c2be1f40af60565a699
nanchao_p <nanchao.p@codehard.co.th>
2025-01-05 14:32:55 +0700

    feat: create update todo with global form_modal_widget

---
+++ lib/pages/my_home/widgets/todo_form_modal_widget.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../models/todo_model.dart';
+
+class TodoFormModalWidget extends StatefulWidget {
+  final bool isEdit;
+  final String? initialTitle;
+  final Priority? initialPriority;
+  final DateTime? initialDueDate;
+  final Function(String, Priority, DateTime) onSave;
+
+  const TodoFormModalWidget({
+    super.key,
+    required this.isEdit,
+    required this.initialTitle,
+    required this.initialPriority,
+    required this.initialDueDate,
+    required this.onSave,
+  });
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TodoFormModalWidgetState createState() {
+    return _TodoFormModalWidgetState();
+  }
+}
+
+class _TodoFormModalWidgetState extends State<TodoFormModalWidget> {
+  late TextEditingController _titleController;
+  late Priority? _selectedPriority;
+  late DateTime? _selectedDueDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.initialTitle);
+    _selectedPriority = widget.initialPriority ?? Priority.low;
+    _selectedDueDate = widget.initialDueDate;
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final title = widget.isEdit ? const Text('Edit') : const Text('Add');
+    final titleIcon = widget.isEdit ? Icons.edit : Icons.add;
+
+    return AlertDialog(
+      title: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              titleIcon,
+              size: 20,
+            ),
+          ),
+          title,
+        ],
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: _titleController,
+            decoration: const InputDecoration(
+              hintText: 'Enter a task',
+              labelText: 'Task Title',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Priority:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ),
+          const SizedBox(height: 5),
+          DropdownButton<Priority>(
+            value: _selectedPriority,
+            onChanged: (value) {
+              if (value != null) {
+                setState(() {
+                  _selectedPriority = value;
+                });
+              }
+            },
+            items: Priority.values.map((priority) {
+              return DropdownMenuItem(
+                value: priority,
+                child: Text(priority.name.toUpperCase()),
+              );
+            }).toList(),
+          ),
+          const SizedBox(height: 15),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Due Date:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ),
+          const SizedBox(height: 5),
+          TextButton(
+            onPressed: () async {
+              final pickedDate = await showDatePicker(
+                context: context,
+                initialDate: _selectedDueDate,
+                firstDate: DateTime.now(),
+                lastDate: DateTime(2100),
+              );
+              if (pickedDate != null) {
+                setState(() {
+                  _selectedDueDate = pickedDate;
+                });
+              }
+            },
+            child: Text(
+              _selectedDueDate == null
+                  ? 'Select Due Date'
+                  : 'Due: ${DateFormat('dd-MMM-yyyy').format(_selectedDueDate!.toLocal())}',
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_titleController.text.isEmpty ||
+                _selectedPriority == null ||
+                _selectedDueDate == null) {
+              return;
+            }
+
+            widget.onSave(
+              _titleController.text,
+              _selectedPriority!,
+              _selectedDueDate!,
+            );
+
+            Navigator.pop(context);
+          },
+          child: const Text('Save'),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/my_home/widgets/todo_list_page.dart
+++ lib/pages/my_home/widgets/todo_list_page.dart
@@ -2,10 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
+import '../../../models/todo_model.dart';
 import '../../../providers/todo_provider.dart';
+import 'todo_form_modal_widget.dart';
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
+
+  void _showEditTodoDialog(
+    BuildContext context,
+    TodoModel todo,
+    int index,
+  ) {
+    showDialog(
+      context: context,
+      builder: (context) => TodoFormModalWidget(
+        initialTitle: todo.title,
+        initialPriority: todo.priority,
+        initialDueDate: todo.dueDate,
+        onSave: (title, priority, dueDate) {
+          Provider.of<TodoProvider>(context, listen: false)
+              .updateTodo(index, title, priority, dueDate);
+        },
+        isEdit: true,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +37,20 @@ class TodoListPage extends StatelessWidget {
       itemCount: todos.length,
       itemBuilder: (context, index) {
         final todo = todos[index];
-        return ListTile(
-          leading: const Icon(Icons.task),
-          title: Text(todo.title),
-          subtitle: Text(DateFormat('dd-MMM-yyyy').format(todo.createdDate)),
+
+        return GestureDetector(
+          onTap: () {
+            _showEditTodoDialog(
+              context,
+              todo,
+              index,
+            );
+          },
+          child: ListTile(
+            leading: const Icon(Icons.task),
+            title: Text(todo.title),
+            subtitle: Text(DateFormat('dd-MMM-yyyy').format(todo.createdDate)),
+          ),
         );
       },
     );
