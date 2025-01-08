3cfd76802f7cd0cc17a2309ae38b3988716a3f02
nanchao_p <nanchao.p@codehard.co.th>
2025-01-08 13:42:18 +0700

    feat: add catch 401 redirect to login

--- lib/interceptors/token_interceptor.dart
+++ lib/interceptors/token_interceptor.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:ghb_todo/routes/app_routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../main.dart';
 
 class TokenInterceptor extends Interceptor {
   String? _token;
@@ -19,5 +25,21 @@ class TokenInterceptor extends Interceptor {
     }
 
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    if (err.response?.statusCode == HttpStatus.unauthorized) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('token');
+
+      navigatorKey.currentState?.pushReplacementNamed(AppRoutes.login);
+      return;
+    }
+
+    super.onError(err, handler);
   }
 }
