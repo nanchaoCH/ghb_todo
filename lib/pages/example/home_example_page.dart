2b60ffb12fc001a35050e278a7b785ea241c03ee
nanchao_p <nanchao.p@codehard.co.th>
2025-01-09 09:07:49 +0700

    feat: add sweden flag

--- lib/pages/example/home_example_page.dart
+++ lib/pages/example/home_example_page.dart
@@ -27,35 +27,104 @@ class ThaiFlag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          const Text('Thai Flag'),
+          Container(
+            height: 200,
             color: Colors.red,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.white,
-          ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Colors.blue,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.white,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.red,
-          ),
-        ),
-      ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: Colors.green,
+                  height: 100,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
+
+    // const yellowWidth = 30.0;
+
+    // return Container(
+    //   color: Colors.white,
+    //   child: Column(
+    //     children: [
+    //       const Text('Sweden Flag'),
+    //       Container(
+    //           height: 100,
+    //           color: Colors.blueAccent,
+    //           child: Row(
+    //             children: [
+    //               Container(
+    //                 color: Colors.blueAccent,
+    //                 width: 100,
+    //               ),
+    //               Container(
+    //                 color: Colors.yellow,
+    //                 width: yellowWidth,
+    //               )
+    //             ],
+    //           )),
+    //       Container(
+    //         color: Colors.yellow,
+    //         height: yellowWidth,
+    //       ),
+    //       Container(
+    //         height: 100,
+    //         color: Colors.blueAccent,
+    //         child: Row(
+    //           children: [
+    //             Container(
+    //               color: Colors.blueAccent,
+    //               width: 100,
+    //             ),
+    //             Container(
+    //               color: Colors.yellow,
+    //               width: yellowWidth,
+    //             )
+    //           ],
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+
+    // return Column(
+    //   children: [
+    //     Expanded(
+    //       child: Container(
+    //         color: Colors.red,
+    //       ),
+    //     ),
+    //     Expanded(
+    //       child: Container(
+    //         color: Colors.white,
+    //       ),
+    //     ),
+    //     Expanded(
+    //       flex: 2,
+    //       child: Container(
+    //         color: Colors.blue,
+    //       ),
+    //     ),
+    //     Expanded(
+    //       child: Container(
+    //         color: Colors.white,
+    //       ),
+    //     ),
+    //     Expanded(
+    //       child: Container(
+    //         color: Colors.red,
+    //       ),
+    //     ),
+    //   ],
+    // );
   }
 }
